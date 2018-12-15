560e12ebcbb13d1ed078e51ee70cc22befb717a2
Martina <tmmartina@yahoo.com>
2018-12-15 15:52:49 -0700

    Void Method Exercise
    Console App that demonstrates Void method

---
+++ MoreMethods/MoreMethods/BasicClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods
+{
+    class BasicClass
+    {
+        
+
+        public void methodOne(int x, int y)
+        {
+            int results = x * 5; 
+            Console.WriteLine(y);
+            
+        }
+    }
+}
---
+++ MoreMethods/MoreMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BasicClass basic = new BasicClass();
+            basic.methodOne(x:10, y:9);
+            Console.ReadLine();
+        }
+    }
+}
