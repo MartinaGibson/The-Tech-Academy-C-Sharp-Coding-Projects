f198de4f19341921ede2d9b4dbad572367293830
Martina <tmmartina@yahoo.com>
2018-12-15 14:31:22 -0700

    Class & Metehods Excercises
    Console Application that demostrations class and simple methods

---
+++ ClassAndMethod/ClassAndMethod/Math.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethod
+{
+    public class Math
+    {
+        
+
+        public int Add(int X)
+        {
+            return X + 10;            
+        }
+
+        public int Multiple(int X)
+        {
+            return X * 2;
+        }
+
+        public int Subtract(int X)
+        {
+            return X - 1;            
+        }
+
+        public int X { get; set; }
+        
+    }
+           
+
+    
+}
---
+++ ClassAndMethod/ClassAndMethod/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethod
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Math mathAdd = new Math();
+            int m = mathAdd.Multiple(input);
+            int a = mathAdd.Add(input);
+            int s = mathAdd.Subtract(input);
+                                             
+            Console.WriteLine(input + " multiple by 2 equals " + m);
+            Console.WriteLine(input + " added to 10 equals " + a);
+            Console.WriteLine(input + " subtracted by 1 equals " + s);
+            Console.ReadLine();
+
+    
+        
+            
+
+            
+            
+
+
+
+            
+            
+
+            
+        
+            
+        }
+    }
+}
+
