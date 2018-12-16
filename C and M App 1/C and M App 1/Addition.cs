ffbc6e9e880e7173dada3bfa1b118347f1e4b406
Martina <tmmartina@yahoo.com>
2018-12-15 17:12:23 -0700

    Overloading Methods
    Console Application demonstrating Overloading Methods

---
+++ C and M App 1/C and M App 1/Addition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_and_M_App_1
+{
+    class Addition
+    {
+        public int GetSum(int a)
+        {
+            return a + a;
+        }
+
+        public decimal GetSum(decimal a)
+        {
+            return Convert.ToInt32(a) + 100;
+        }
+
+        public string GetSum(string a)
+        {
+            int aa = Convert.ToInt32(a) + 10;
+            return aa.ToString();       
+        }
+    }
+}
---
+++ C and M App 1/C and M App 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_and_M_App_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Addition add = new Addition();
+
+            Console.WriteLine(add.GetSum(12));
+            Console.ReadLine();
+
+            Addition addDec = new Addition();
+
+            Console.WriteLine(add.GetSum(2.5m));
+            Console.ReadLine();
+
+            Addition addString = new Addition();
+
+            Console.WriteLine(add.GetSum("3"));
+            Console.ReadLine();
+
+        }
+    }
+}
