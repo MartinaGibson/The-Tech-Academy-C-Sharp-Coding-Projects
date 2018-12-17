de456c23bb45856314d23ec2643699c60c6813e7
Martina <tmmartina@yahoo.com>
2018-12-17 08:26:58 -0700

    Optional Params & Out Param
    Console Application that demonstrates optional parameters and an out parameter.

---
+++ C and M Console App 2/C and M Console App 2/BasicClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_and_M_Console_App_2
+{
+    public class BasicClass
+    {
+        public int optionalparams(int x, int y = 0)
+        {
+            return x + Convert.ToInt32(y);            
+        }
+         
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+
+}
---
+++ C and M Console App 2/C and M Console App 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_and_M_Console_App_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicClass basic = new BasicClass();
+
+            Console.WriteLine("Please enter a number.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a another number IF YOU WANT TO.");
+            int y;
+            int results;
+            if(int.TryParse(Console.ReadLine(), out y))
+            {
+                results = basic.optionalparams(x, y);
+            }
+            else
+            {
+                results = basic.optionalparams(x);
+            }
+            Console.WriteLine(results+" is the sum!");
+            Console.ReadLine();
+
+            
+
+
+
+        }
+    }
+}
