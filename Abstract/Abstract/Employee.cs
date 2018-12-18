0fffe0fdd0e8aae8a6780d9cccc5e156966a540d
Martina <tmmartina@yahoo.com>
2018-12-18 15:33:00 -0700

    Abstract Class & Method
    Simple Console demonstrating a Abstract Class and Method

---
+++ Abstract/Abstract/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    public class Employee : Person
+    {
+        int Id { get; set; }
+        
+
+        public override void SayName()
+        {
+            Console.WriteLine("My Name is ");
+            base.SayName();
+        }
+
+    }
+}
