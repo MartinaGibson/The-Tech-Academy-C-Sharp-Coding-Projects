ddadf50411f7135de37c70c325ae5b8c939649eb
Martina <tmmartina@yahoo.com>
2018-12-25 14:39:26 -0700

    DateTime
    A simple application that demonstrates use od DateTime date types.

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDate = DateTime.Now;
+            Console.WriteLine("Its is currently "+ currentDate);
+            Console.WriteLine("Enter a number");
+            double input = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("It will be {0} {1} hours from now.",currentDate.AddHours(input) ,input);            
+            Console.ReadLine();
+           
+        }
+    }
+}
