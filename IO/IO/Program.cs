d229db1429ec0506c6e127aff51ed7918b8a83c7
Martina <tmmartina@yahoo.com>
2018-12-25 11:22:22 -0700

    Using System.IO;
    Console Application demonstrating inputing and outputing files

---
+++ IO/IO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter in a number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            File.WriteAllText(@"C:\Users\tmmar\Logs\logIO.txt", Convert.ToString(number));
+            Console.Write("Your number is ");
+            Console.WriteLine(File.ReadAllText(@"C:\Users\tmmar\Logs\logIO.txt"));
+            Console.ReadLine();
+        }
+    }
+}
