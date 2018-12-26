c27e7472c7b1608e3aacff541bf098a10d01df21
Martina <tmmartina@yahoo.com>
2018-12-26 07:59:34 -0700

    Additional Features Exercise
    Demostrates Constructor Chaining, the use of Var and declaring a constant

---
+++ AddtlFeatures/AddtlFeatures/Peeps.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddtlFeatures
+{
+    class Peeps
+    {
+        public Peeps()
+        {
+
+        }
+        public Peeps (string personName)
+        {
+            this.personName = personName;
+        }
+        public Peeps(int personAge) : this("Who is this? I didn't here the name!")
+        {
+            
+        }     
+           
+
+        public string personName{ get; set; }
+        public int personAge { get; set; }    
+        
+
+        
+        
+    }
+}
---
+++ AddtlFeatures/AddtlFeatures/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddtlFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string companyName = "Last Minute Sitters";
+            Console.WriteLine("Welcome to {0}! What is the age of your child?", companyName);
+            var personAge1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is first name of this child?");
+            var personName1 = Console.ReadLine();
+            Console.WriteLine("What is the name of the 2nd child?");
+            var personName2 = Console.ReadLine();
+            Peeps child1 = new Peeps(personAge1);
+            Peeps child2 = new Peeps(personName2);
+
+            Console.WriteLine("The first child's age is "+personAge1+ " and again "+ child1.personName);
+            Console.WriteLine("Oh that's " + personName1);
+            Console.WriteLine("The second child's name is "+child2.personName);
+            Console.ReadLine();
+            Console.WriteLine("What is " + child2.personName + "age?");
+            var personAge2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(child2.personName + " is " + personAge2);
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
